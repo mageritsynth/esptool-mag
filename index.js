9d0ed37240aba8c7665d8de75e44b9978aa2dbe9
mageritsynth <77498459+mageritsynth@users.noreply.github.com>
2023-02-26 00:46:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const connectButton = document.getElementById('connectButton');
+
+connectButton.onclick = async () => {
+
+  console.log('Settings done for :' + chip);
+  connectButton.style.display = 'none';
+};
