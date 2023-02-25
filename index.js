8ac32ac7baf2d7cb43b6824edb22e630ca54786a
mageritsynth <77498459+mageritsynth@users.noreply.github.com>
2023-02-26 00:49:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const connectButton = document.getElementById('connectButton');
 
 connectButton.onclick = async () => {
 
-  console.log('Settings done for :' + chip);
+  console.log('Settings done for :');
   connectButton.style.display = 'none';
 };
